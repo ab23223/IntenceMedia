51d1055594aaf4d25aaf8818873604ffe2319483
Ashton Balu <ab23223@students.sacredheart.school.nz>
2025-05-20 00:09:45 +0000

    save

--- app.py
+++ app.py
@@ -1,5 +1,28 @@
-from bottle import run,route,template, static_file
+from bottle import run,route,template, static_file, response
 import requests
+import cloudinary
+import cloudinary.api
+import json
+
+# 🔐 Your Cloudinary credentials
+cloudinary.config(
+    cloud_name='dfqreujbo',
+    api_key='467879367759351',
+    api_secret='tgJspwPABIOzKQrG3YSeb7YAx2g'
+)
+
+@route('/images')
+def get_images():
+    folder = 'Training 15-05'  # Your Cloudinary folder
+    try:
+        result = cloudinary.api.resources(type='upload', prefix=f'{folder}/', max_results=100)
+        urls = [item['secure_url'] for item in result['resources']]
+        response.content_type = 'application/json'
+        return json.dumps(urls)
+    except Exception as e:
+        response.status = 500
+        return str(e)
+
 
 @route('/static/<filepath:path>')
 def server_static(filepath):
@@ -18,6 +41,10 @@ def categorysports():
 @route('/category-automotive')
 def categoryautomotive():
      return template('category-automotive')
+
+@route('/Training-15-05')
+def Training1505():
+     return template('Training-15-05')
 
 
 @route('/about')
